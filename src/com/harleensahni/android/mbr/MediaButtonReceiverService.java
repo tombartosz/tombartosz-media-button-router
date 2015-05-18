4e54bc43aa0ce14e5fddef2ca418f22c4799fc0e
tombartosz <tombartosz@gmail.com>
2015-05-18 17:09:26 +0200

    Some refactoring and screen wake fix

--- src/com/harleensahni/android/mbr/MediaButtonReceiverService.java
+++ src/com/harleensahni/android/mbr/MediaButtonReceiverService.java
@@ -1,6 +1,5 @@
 package com.harleensahni.android.mbr;
 
-import static com.harleensahni.android.mbr.Constants.*;
 import static com.harleensahni.android.mbr.Constants.TAG;
 
 import java.util.ArrayList;
@@ -19,10 +18,7 @@ import android.content.pm.ResolveInfo;
 import android.media.AudioManager;
 import android.os.PowerManager;
 import android.os.PowerManager.WakeLock;
-
-
 import android.util.Log;
-import android.view.KeyEvent;
 
 import com.harleensahni.android.mbr.utils.Preferences;
 
@@ -52,6 +48,62 @@ public class MediaButtonReceiverService extends IntentService {
         AudioManager audioManager = ((AudioManager) this.getSystemService(Context.AUDIO_SERVICE));
         return audioManager.isMusicActive();
     }
+
+    private ComponentName guessMediaPlayer() {
+        // Try to best guess who is playing the music based off
+        // of
+        // running foreground services.
+
+        // XXX Move stuff like receivers to service so we can
+        // cache
+        // it. Doing too much stuff here
+        List<ResolveInfo> receivers = Utils.getMediaReceivers(this.getPackageManager(), false, null);
+
+        // Remove our app from the list so users can't select
+        // it.
+        if (receivers != null) {
+
+            ActivityManager activityManager = ((ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE));
+            List<RunningServiceInfo> runningServices = activityManager
+                    .getRunningServices(Integer.MAX_VALUE);
+
+            List<RunningServiceInfo> candidateServices = new ArrayList<ActivityManager.RunningServiceInfo>();
+            for (RunningServiceInfo runningService : runningServices) {
+                if (runningService.started && runningService.foreground) {
+                    candidateServices.add(runningService);
+                }
+            }
+
+            for (ResolveInfo resolveInfo : receivers) {
+                // Find any service that's package matches that
+                // of a receivers.
+                for (RunningServiceInfo candidateService : candidateServices) {
+
+                    if (candidateService.foreground &&
+                            candidateService.started
+                            && resolveInfo.activityInfo.packageName.equals(candidateService.service
+                            .getPackageName())) {
+
+                        //We found a match...
+
+                        //Set the current audio player name/package
+                        Preferences.getPreferences().edit().putString(Constants.CURRENT_AUDIO_PLAYER_NAME, resolveInfo.activityInfo.name)
+                                .putString(Constants.CURRENT_AUDIO_PLAYER_PACKAGE, resolveInfo.activityInfo.packageName)
+                                .commit();
+
+                        String packageName = resolveInfo.activityInfo.packageName;
+                        String activityName = resolveInfo.activityInfo.name;
+
+                        Log.i(TAG, "Media Button Receiver Service: Music playing and passed on event  "
+                                + " to " + resolveInfo.activityInfo.name);
+
+                        return new ComponentName(packageName, activityName);
+                    }
+                }
+            }
+        }
+        return null;
+    }
 	
 	@Override
 	protected void onHandleIntent(Intent intent) {	
@@ -61,17 +113,15 @@ public class MediaButtonReceiverService extends IntentService {
 
         if (isSelectorAlreadyVisible) {
             /* If selector is already visible - forward intent to it */
-            Intent receiverSelectorIntent = new Intent(INTENT_ACTION_VIEW_MEDIA_LIST_KEYPRESS);
+            Intent receiverSelectorIntent = new Intent(Constants.INTENT_ACTION_VIEW_MEDIA_LIST_KEYPRESS);
             receiverSelectorIntent.putExtras(intent);
             context.sendBroadcast(receiverSelectorIntent);
             return;
         }
 
-
-        int keyCode = intent.getIntExtra(INTENT_KEY_CODE, 0);
+        int keyCode = intent.getIntExtra(Constants.INTENT_KEY_CODE, 0);
 
         Log.i(TAG, "MediaButtonReceiverService: handling legitimate media key code: " + keyCode);
-
 
         //SCENARIO 1: Music is currently playing. 
         if (checkIsMusicActive()) {
@@ -81,102 +131,35 @@ public class MediaButtonReceiverService extends IntentService {
 
                 Log.d(TAG, "Last media button receiver stored in preferences is null");
 
-                // Try to best guess who is playing the music based off
-                // of
-                // running foreground services.
+                ComponentName currentlyPlayingComponent = this.guessMediaPlayer();
 
-                // XXX Move stuff like receivers to service so we can
-                // cache
-                // it. Doing too much stuff here
-                List<ResolveInfo> receivers = Utils.getMediaReceivers(context.getPackageManager(), false, null);
+                if (currentlyPlayingComponent != null) {
+                    Utils.forwardKeyCodeToComponent(context, currentlyPlayingComponent, false, keyCode, null);
+                }
+                else {
+                    if (Preferences.getPreferences().getBoolean(Constants.CONSERVATIVE_PREF_KEY, false)) {
+                        //DAS - Don't set ANY CURRENT_AUDIO_PLAYER value. This means we'll
+                        //default to the Voice Command module, which isn't ideal, but it's
+                        //better than nothing.
 
-                // Remove our app from the list so users can't select
-                // it.
-                if (receivers != null) {
-                    ActivityManager activityManager = ((ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE));
-                    List<RunningServiceInfo> runningServices = activityManager
-                            .getRunningServices(Integer.MAX_VALUE);
-               
+                        //If there's a previously set 'current audio player'
+                        //property, we'll use that in the selector.
 
-                    // Only need to look at services that are foreground
-                    // and started            
-                    
-                    List<RunningServiceInfo> candidateServices = new ArrayList<ActivityManager.RunningServiceInfo>();
-                    for (RunningServiceInfo runningService : runningServices) {
-                        if (runningService.started && runningService.foreground) {
-                            candidateServices.add(runningService);
-                        }
-                    }
+                        Log.i(TAG, "Media Button Receiver Service: No Receivers found playing music. Intent broadcast will be aborted.");
 
-                    boolean matched = false;
-                    
-                    for (ResolveInfo resolveInfo : receivers) {
-                        
-                        // Find any service that's package matches that
-                        // of a receivers.
-                        for (RunningServiceInfo candidateService : candidateServices) {
-
-                            if (candidateService.foreground &&
-                            	candidateService.started
-                                && resolveInfo.activityInfo.packageName.equals(candidateService.service
-                                            .getPackageName())) {
-                            	
-                            	//We found a match...
-
-                                //Set the current audio player name/package
-                                Preferences.getPreferences().edit().putString(CURRENT_AUDIO_PLAYER_NAME, resolveInfo.activityInfo.name)
-                                                  .putString(CURRENT_AUDIO_PLAYER_PACKAGE, resolveInfo.activityInfo.packageName)
-                                                  .commit();
-
-                                String packageName = resolveInfo.activityInfo.packageName;
-                                String activityName = resolveInfo.activityInfo.name;
-
-                                Utils.forwardKeyCodeToComponent(context,
-                                        new ComponentName(packageName,
-                                                activityName), false, keyCode, null);
-                                    
-
-                             
-                                matched = true;
-                                Log.i(TAG, "Media Button Receiver Service: Music playing and passed on event : "
-                                        + keyCode + " to " + resolveInfo.activityInfo.name);
-                                break;
-                            }
-                        }
-                        if (matched) {
-                            // TODO Need to handle case with multiple
-                            // matches, maybe by showing selector
-                            break;
-                        }
-                    }
-                    
-                    if (!matched) {
-                        if (Preferences.getPreferences().getBoolean(CONSERVATIVE_PREF_KEY, false)) {
-                            //DAS - Don't set ANY CURRENT_AUDIO_PLAYER value. This means we'll
-                        	//default to the Voice Command module, which isn't ideal, but it's
-                        	//better than nothing.
-                        	
-                        	//If there's a previously set 'current audio player'
-                        	//property, we'll use that in the selector.
-                            
-                        	Log.i(TAG, "Media Button Receiver Service: No Receivers found playing music. Intent broadcast will be aborted.");
-
-                            showSelector(context, intent);
+                        showSelector(context, intent);
 
 
-                        } else {
-                            Log.i(TAG, "Media Button Receiver Service: No Receivers found playing music. Intent will use regular priorities.");
-                            //DAS - Since we moved this code to a service, that means we have to resend 
-                            //the ordered broadcast; so as to avoid an endless loop, we need to make
-                            //sure we set a flag that tells our broadcast receiver to ignore this intent.
+                    } else {
+                        Log.i(TAG, "Media Button Receiver Service: No Receivers found playing music. Intent will use regular priorities.");
+                        //DAS - Since we moved this code to a service, that means we have to resend
+                        //the ordered broadcast; so as to avoid an endless loop, we need to make
+                        //sure we set a flag that tells our broadcast receiver to ignore this intent.
 
-                            Utils.forwardKeyCodeToComponent(context, null, false, keyCode, null, true);
+                        Utils.forwardKeyCodeToComponent(context, null, false, keyCode, null, true);
 
-                        }
                     }
                 }
-                return;
-                
             } else {
             	//SCENARIO 2: Audio is playing AND code is for ICS...
 
@@ -184,35 +167,34 @@ public class MediaButtonReceiverService extends IntentService {
                 ComponentName cn = ComponentName.unflattenFromString(lastMediaButtonReceiverName);
 
                 //Set the current audio player name/package
-                Preferences.getPreferences().edit().putString(CURRENT_AUDIO_PLAYER_NAME, cn.getShortClassName())
-                                  .putString(CURRENT_AUDIO_PLAYER_PACKAGE, cn.getPackageName())
+                Preferences.getPreferences().edit().putString(Constants.CURRENT_AUDIO_PLAYER_NAME, cn.getShortClassName())
+                                  .putString(Constants.CURRENT_AUDIO_PLAYER_PACKAGE, cn.getPackageName())
                                   .commit();
 
                 Utils.forwardKeyCodeToComponent(context,
                         ComponentName.unflattenFromString(lastMediaButtonReceiverName), false, keyCode, null);
-
-                return;
             }
         }
+        else {
 
-        //SCENARIO 3: No music is playing. A music player might be paused (in which case, we'll
-        //figure it out from the properties we previously set) or, if we don't have any info about
-        //a music player, we'll assume voice commands.
-        
+            //SCENARIO 3: No music is playing. A music player might be paused (in which case, we'll
+            //figure it out from the properties we previously set) or, if we don't have any info about
+            //a music player, we'll assume voice commands.
 
-        List<ResolveInfo> receivers = Utils.getAllReceivers(context.getPackageManager(), true, false, context);
-        if (receivers.size() == 1) {
-            // Not using last last_media_button_receiver
-            // since we want this feature to work just as
-            // well on Android version < 4.0
-            ResolveInfo resolveInfo = receivers.get(0);
-            Utils.forwardKeyCodeToComponent(context, new ComponentName(
-                    resolveInfo.activityInfo.packageName, resolveInfo.activityInfo.name), false,
-                    keyCode, null);
-        } else {
-            showSelector(context, intent);
+
+            List<ResolveInfo> receivers = Utils.getAllReceivers(context.getPackageManager(), true, false, context);
+            if (receivers.size() == 1) {
+                // Not using last last_media_button_receiver
+                // since we want this feature to work just as
+                // well on Android version < 4.0
+                ResolveInfo resolveInfo = receivers.get(0);
+                Utils.forwardKeyCodeToComponent(context, new ComponentName(
+                                resolveInfo.activityInfo.packageName, resolveInfo.activityInfo.name), false,
+                        keyCode, null);
+            } else {
+                showSelector(context, intent);
+            }
         }
-
 	}
 	
 	 /**
@@ -228,25 +210,16 @@ public class MediaButtonReceiverService extends IntentService {
         KeyguardManager manager = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
         boolean locked = manager.inKeyguardRestrictedInputMode();
 
-        Intent showForwardView = new Intent(INTENT_ACTION_VIEW_MEDIA_BUTTON_LIST);
+        Intent showForwardView = new Intent(Constants.INTENT_ACTION_VIEW_MEDIA_BUTTON_LIST);
         showForwardView.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         showForwardView.putExtras(intent);
         showForwardView.setClassName(context,
                 locked ? ReceiverSelectorLocked.class.getName() : ReceiverSelector.class.getName());
-
-
         if (locked) {
-
-            // XXX See if this actually makes a difference, might
-            // not be needed if we move more things to onCreate?
             PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-            // acquire temp wake lock
             WakeLock wakeLock = powerManager.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP
                     | PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, TAG);
             wakeLock.setReferenceCounted(false);
-
-            // Our app better display within 3 seconds or we have
-            // bigger issues.
             wakeLock.acquire(Constants.WAKE_TIME);
         }
         context.startActivity(showForwardView);
